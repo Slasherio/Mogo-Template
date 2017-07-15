ee8da003bf3d093fedf35c0f520224797944553f
Oleksandr Myshchyshyn <vbobiby.my@gmail.com>
2017-07-16 02:56:38 +0300

    Add scripts for menu

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,13 @@
 //Accordion
-jQuery(document).ready(function() {
+$(document).ready(function() {
     function close_accordion_section() {
-        jQuery('.service-content__accordion .service-content__accordion-section_title').removeClass('active');
-        jQuery('.service-content__accordion .service-content__accordion-section_text').slideUp(300).removeClass('open');
+        $('.service-content__accordion .service-content__accordion-section_title').removeClass('active');
+        $('.service-content__accordion .service-content__accordion-section_text').slideUp(300).removeClass('open');
     }
 
-    jQuery('.service-content__accordion-section_title').click(function(e) {
+    $('.service-content__accordion-section_title').click(function(e) {
         // Grab current anchor value
-        var currentAttrValue = jQuery(this).attr('href');
+        var currentAttrValue = $(this).attr('href');
 
         if (jQuery(e.target).is('.active')) {
             close_accordion_section();
@@ -15,26 +15,43 @@ jQuery(document).ready(function() {
             close_accordion_section();
 
             // Add active class to section title
-            jQuery(this).addClass('active');
+            $(this).addClass('active');
             // Open up the hidden content panel
-            jQuery('.service-content__accordion ' + currentAttrValue).slideDown(300).addClass('open');
+            $('.service-content__accordion ' + currentAttrValue).slideDown(300).addClass('open');
         }
 
         e.preventDefault();
     });
-});
 
-//Slider
-$('.slider__wrapper').slick({
-    arrows: true,
-    dots: false,
-    autoplay: true,
-    autoplaySpeed: 2000
-});
+    //Slider
+    $('.slider__wrapper').slick({
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
 
-//Testimonial slider
-$('.testimonials__wrapper').slick({
-    arrow: true,
-    dots: false,
-    autoplay: true
+    //Testimonial slider
+    $('.testimonials__wrapper').slick({
+        arrow: true,
+        dots: false,
+        autoplay: true
+    });
+
+    $('.navigation__link').click(function(e) {
+        var link = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 1500);
+        e.preventDefault();
+    });
+
+    $(".burger-menu").click(function(e) {
+        $(this).toggleClass("menu-on");
+        $('.burger-list').fadeToggle('burger-list-active');
+        $('.burger_elem').click(function(e) {
+            $(".burger-menu").toggleClass("menu-on");
+            $('.burger-list').fadeToggle('burger-list-active');
+        });
+    });
 });
